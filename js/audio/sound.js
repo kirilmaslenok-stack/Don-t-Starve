012e0e1723dc440e8c0cd33b0ded25961ece84dc
AnaliniDebozhir228 <anya.maw16@gmail.com>
2026-03-25 12:35:35 +0300

    Update sound.js

--- js/audio/sound.js
+++ js/audio/sound.js
@@ -1,4 +1,7 @@
 function helloSound() {
+    const audio = document.getElementById('testSound');
+    console.log("Sound & Effects ready 🔊 (звук загружен: " + !!audio + ")");
+    
     console.log("🔊 Sound & Effects ready");
 }
 
