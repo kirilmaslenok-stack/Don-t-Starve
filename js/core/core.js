664d80d5765e5891be06cf28c6626ea63f96f090
GEAR ENCORE <ila289193@gmail.com>
2026-04-15 11:43:16 +0300

    Update core.js
    start

--- js/core/core.js
+++ js/core/core.js
@@ -9,4 +9,22 @@ class CoreGame {
         this.camera = camera;
         this.lastTimestamp = 0;
     }
+    // Добавьте в класс CoreGame
+
+    start() {
+    
+        this.lastTimestamp = 0;
+    
+        console.log("🎮 Game loop started");
+    
+    // Запускаем фоновую музыку через 1 секунду
+    
+        setTimeout(() => {
+        
+            this.soundManager.playMusic('ambient', 0.3);
+    
+        }, 1000);
+}
+
+    
 }
